8c71d3c40ba2470422639b5a79e89dcd4a66d0b7
junsub123 <89231026+junsub123@users.noreply.github.com>
2024-03-12 16:29:31 +0900

    컴포넌트 구현 및 node_modules gitignore
    node_modules gitignore
    
    gitignore changes
    
    [create]: Footer 구현

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/index';
-import App from './pages/test/test';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Login from './pages/auth/login';
+import Footer from './components/layout/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Home />
-      <App/>
+      <Login />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, Stack, Typography } from '@mui/material';
+import TextInput from '../../components/Form/TextInput';
 
 const Login = () => {
-    return(
-        <div>
-            
-        </div>
+    const [id, setId] = useState('');
+    const [pwd, setPwd] = useState('');
+
+    return (
+        <Box sx={{
+            width: '600px',
+            height: '800px'
+        }}>
+            <Typography variant='h3' component="h3">로그인</Typography>
+            <Stack>
+                <TextInput onChange={(e) => setId(e.target.value)} value={id}/>
+                <TextInput onChange={(e) => setPwd(e.target.value)} value={pwd}/>
+            </Stack>
+
+            <Button>로그인</Button>
+        </Box >
     )
 }
 
