31bd172cda6047c6a796330f155fd073985f13f8
junsub123 <89231026+junsub123@users.noreply.github.com>
2024-03-12 12:48:16 +0900

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <Link>안녕</Link>
+      </header>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default Home;
