1f096b74558ebe675041d1a3e270395c4ad413cc
gimjuncheol <85679149+jammykii@users.noreply.github.com>
2024-03-27 16:18:38 +0900

    주문 내역 api 연결

--- src/components/OrdersCard.jsx
+++ src/components/OrdersCard.jsx
@@ -2,18 +2,23 @@ import { Box, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import dress from '../assets/dress.jpg'
 
-const OrdersCard = () => {
+const OrdersCard = (product) => {
     return (
-        <Box sx={{ display: 'flex', mt: 3, width: '100%', border: '1px solid #DEDEDE', p: 3, borderRadius: 4 }}>
-            <img src={dress} alt={dress} style={{ width: 130, height: 130, borderRadius: 15 }} />
+        <div>
+        {product.product.map((item)=>{
+            return(
+            <Box sx={{ display: 'flex', mt: 3, width: '100%', border: '1px solid #DEDEDE', p: 3, borderRadius: 4 }}>
+            <img src={`assets/${item.orderItems[0].goods_no.gimg}`} alt={dress} style={{ width: 130, height: 130, borderRadius: 15 }} />
             <Box sx={{ ml: 3 }}>
-                <NavLink style={{ textDecoration: 'none' }} to={`/productdetail/${1}`}>
-                    <Typography sx={{ lineHeight: 2, mt: 2, color: 'black' }}>블랙 드레스</Typography>
+                <NavLink style={{ textDecoration: 'none' }} to={`/productdetail/${item.orderItems[0].goods_no.gno}`}>
+                    <Typography sx={{ lineHeight: 2, mt: 2, color: 'black' }}>{item.orderItems[0].goods_no.gname}</Typography>
                 </NavLink>
-                <Typography sx={{ lineHeight: 2 }}>2개</Typography>
-                <Typography sx={{ lineHeight: 2, fontSize: 20 }} fontWeight={600}>500원</Typography>
+                <Typography sx={{ lineHeight: 2 }}> {item.orderItems[0].ordQty}개</Typography>
+                <Typography sx={{ lineHeight: 2, fontSize: 20 }} fontWeight={600}>{item.toPrc}원</Typography>
             </Box>
         </Box>
+        )})}
+        </div>
     )
 }
 
--- src/pages/Myinfo.jsx
+++ src/pages/Myinfo.jsx
@@ -1,45 +1,42 @@
 import { Grid, Typography, Box } from '@mui/material';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import MyinfoNavList from '../components/MyinfoNavList';
-import OrderListCard from '../components/OrderListCard';
-import { useParams } from 'react-router-dom';
+import OrdersCard from '../components/OrdersCard';
 import axios from 'axios';
 
+
 const Myinfo = () => {
-    const param = useParams()
     const [name, setName] = useState('');
-    const [title, setTitle] = useState('');
-    const [price, setPrice] = useState('');
-    const [qty, setQty] = useState('');
-    const [date, setDate] = useState('');
+    const [orders, setOrders] = useState([]);
 
     useEffect(() => {
         axios.post('/members/loginCheck')
             .then((res) => {
-                setName(res.data)
+                setName(res.data);
             })
-            .catch((err) => {
-                console.log(err)
-            })
-    })
+            .catch((error) => {
+                console.error('Error checking login status:', error);
+            });
+    });
 
     useEffect(() => {
-        axios.get(`/orders/orderList?id=${name}`)
+        if(name !== undefined){
+            axios.get(`/orders/orderList?id=${name}`)
             .then((res) => {
-                console.log(res)
+                setOrders(res.data.content)
             })
-            .catch((err) => {
-                console.log(err)
-            })
-    }, [name])
-
+            .catch((error) => {
+                console.error('Error checking login status:', error);
+            });
+        }
+    },[]);
     return (
         <Box sx={{ display: 'flex' }}>
             <MyinfoNavList />
             <Box>
                 <Typography sx={{mb: 3}} variant='h5' fontWeight={600}>주문 내역</Typography>
                 <Grid sx={{ width: 700 }}>
-                    <OrderListCard />
+                    {orders.length > 0? <OrdersCard product={orders}/>:null}
                 </Grid>
             </Box>
         </Box>
