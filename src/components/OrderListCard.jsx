5f1e66dfc52af91fd3613a6ebf53ca25b99ff71a
gimjuncheol <85679149+jammykii@users.noreply.github.com>
2024-03-29 16:12:13 +0900

    Update OrderListCard.jsx
    주문 내역 컴포넌트 수정

--- src/components/OrderListCard.jsx
+++ src/components/OrderListCard.jsx
@@ -4,6 +4,7 @@ import { Box, Typography } from '@mui/material';
 
 
 const OrderListCard = ({ product }) => {
+    console.log(product)
     return (
         <div>
             {product.map((item, key) => {
@@ -13,14 +14,14 @@ const OrderListCard = ({ product }) => {
                     <Box key={key} sx={{ borderRadius: 4, p: 2, boxShadow: 5, mb: 3 }}>
                         <Typography sx={{ fontWeight: 600, fontSize: 20, mb: 2 }}>{item.ordDt} 주문</Typography>
                         <Box sx={{ display: 'flex', border: '1px solid #DEDEDE', borderRadius: 4, p: 2 }}>
-                            <img src={`assets/${item.orderItems[0].goods_no.gimg}`} alt={`assets/${item.orderItems[0].goods_no.gimg}`} style={{ width: '130px', height: '130px' }} />
+                            <img src={`assets/${item.orderItem[0].goods_no.gimg}`} alt={`assets/${item.orderItem[0].goods_no.gimg}`} style={{ width: '130px', height: '130px' }} />
                             <Box sx={{ display: 'flex', flexDirection: 'column', width: 300 }}>
                                 <Box sx={{ height: 100 }}>
-                                    <Typography sx={{ ml: 3 }}>{item.orderItems[0].goods_no.gname}</Typography>
+                                    <Typography sx={{ ml: 3 }}>{item.orderItem[0].goods_no.gname}</Typography>
                                 </Box>
                                 <Box sx={{ ml: 3, display: 'flex' }}>
-                                    <Typography sx={{ color: 'gray', mr: 5 }}>{price}원 - {item.orderItems[0].ordQty}개</Typography>
-                                    <Typography sx={{ color: 'gray' }}>옵션: {item.orderItems[0].item_no.optVal1} {item.orderItems[0].item_no.optVal2 ? "- " + item.orderItems[0].item_no.optVal2 : null}</Typography>
+                                    <Typography sx={{ color: 'gray', mr: 5 }}>{price}원 - {item.orderItem[0].ordQty}개</Typography>
+                                    <Typography sx={{ color: 'gray' }}>옵션: {item.orderItem[0].item_no.optVal1} {item.orderItem[0].item_no.optVal2 ? "- " + item.orderItem[0].item_no.optVal2 : null}</Typography>
                                 </Box>
                             </Box>
                             <Box sx={{ borderLeft: '1px solid #DEDEDE', pl: 3 }}>
